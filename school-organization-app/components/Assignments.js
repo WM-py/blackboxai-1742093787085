cffe0550a9b7b815927e28eba05ffcc422a582ab
Watylla Macedo <83824090+WM-py@users.noreply.github.com>
2025-03-15 23:56:39 -0300

    Add school-organization-app/components/Assignments.js 1742093798936

---
+++ school-organization-app/components/Assignments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Assignments = () => {
+    return (
+        <div>
+            <h2>Assignments and Projects</h2>
+            {/* Assignments will be displayed here */}
+        </div>
+    );
+};
+
+export default Assignments;

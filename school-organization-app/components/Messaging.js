0a51ccd02a06080c3c74a02f015309475cf5161c
Watylla Macedo <83824090+WM-py@users.noreply.github.com>
2025-03-15 23:56:37 -0300

    Add school-organization-app/components/Messaging.js 1742093797014

---
+++ school-organization-app/components/Messaging.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Messaging = () => {
+    return (
+        <div>
+            <h2>Messaging</h2>
+            {/* Messaging interface will be displayed here */}
+        </div>
+    );
+};
+
+export default Messaging;
